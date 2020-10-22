7f9056e95b12934f3be1e1cb3621284695a87e1b
int86 <273206894@qq.com>
2020-10-22 10:22:55 +0800

    database

---
+++ 数据库/596. 超过5名学生的课.sql
@@ -0,0 +1,33 @@
+-- SQL架构
+-- 有一个courses 表 ，有: student (学生) 和 class (课程)。
+
+-- 请列出所有超过或等于5名学生的课。
+
+-- 例如，表：
+
+-- +---------+------------+
+-- | student | class      |
+-- +---------+------------+
+-- | A       | Math       |
+-- | B       | English    |
+-- | C       | Math       |
+-- | D       | Biology    |
+-- | E       | Math       |
+-- | F       | Computer   |
+-- | G       | Math       |
+-- | H       | Math       |
+-- | I       | Math       |
+-- +---------+------------+
+-- 应该输出:
+
+-- +---------+
+-- | class   |
+-- +---------+
+-- | Math    |
+-- +---------+
+
+SELECT
+    class
+FROM
+    (select class,count(DISTINCT student) as num from courses group by class) as temp_table
+where num >= 5
